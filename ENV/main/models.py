70650d90909b0e46748ff8406c098893b5411615
Freestylecmg <freestylecmg@gmail.com>
2022-03-26 18:54:27 -0700

    2

--- ENV/main/models.py
+++ ENV/main/models.py
@@ -1,3 +1,12 @@
+import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Day(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = datetime.date.today()  
+    trash = models.FloatField(max_length=1000000)
+    water = models.FloatField(max_length=1000000)
+    co2 = models.FloatField(max_length=1000000)
+    energy = models.FloatField(max_length=1000000)
--- ENV/main/views.py
+++ ENV/main/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+import email
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Oops! Credentials Invalid')
+            return redirect('login')
+    else:
+     return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(email=email).exists():
+                messages.info(request, 'Oops! Email Already Used')
+                return redirect('register')
+        elif User.objects.filter(username=username).exists():
+            messages.info(request, 'Oops! Username Already Used') 
+            return redirect('register') 
+        else:
+            user = User.objects.create_user(username = username, email = email, password = password)
+            user.save(); 
+            return redirect('login')
+    else:
+         return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+@login_required(login_url='login')
+def log(request):
+    return render(request, 'log.html')
+
+@login_required(login_url='login')
+def stats(request):
+    return render(request, 'stats.html')
